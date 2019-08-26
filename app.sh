23acb4c26fb79442d129c9a737c1f022e81a2604
Rodrigo Hernández <rodrigo19-1_1998@hotmail.com>
2019-08-26 03:13:29 -0300

    Create app.sh

---
+++ app.sh
@@ -0,0 +1,21 @@
+docker network create dev
+cd api-gdd
+docker build --tag api-gdd --network dev .
+docker pull erlang
+docker pull rabbitmq:3.7.17
+docker pull mysql:8.0
+cd ..
+cd mq
+docker build --tag mq --network dev .
+cd ..
+cd api-gdc
+docker build --tag api-gdc --network dev .
+docker run -d -p 3307:33060 --network dev --name mysql-gdc -e MYSQL_ROOT_PASSWORD=19+Enero+1998 -e MYSQL_DATABASE=gdc -e MYSQL_USER=api -e MYSQL_PASSWORD=api..gdc++19  mysql:8.0
+sleep 2m
+docker run -d -p 3000:3000 --network dev --name apigdd api-gdd
+sleep 20
+docker run -d -p 5672:5672 --network dev --name mqservice rabbitmq:3.7.17
+sleep 30
+docker run -d -p 5000:5050 --network dev --name api-gdc api-gdc
+sleep 20
+docker run --network dev --name mq mq
