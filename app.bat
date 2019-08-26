f8bce29e6dc8cc864b08eeeb751ac2826b174677
Rodrigo Hernández <rodrigo19-1_1998@hotmail.com>
2019-08-26 04:04:18 -0300

    Update app.bat

--- app.bat
+++ app.bat
@@ -1,21 +1,21 @@
 docker network create dev
 cd api-gdd
-docker build --tag api-gdd --network dev .
+docker build --tag api-gdd --rmi --network dev .
 docker pull erlang
 docker pull rabbitmq:3.7.17
 docker pull mysql:8.0
 cd..
 cd mq
-docker build --tag mq --network dev .
+docker build --tag mq --rmi --network dev .
 cd..
 cd api-gdc
-docker build --tag api-gdc --network dev .
-docker run -d -p 3307:33060 --network dev --name mysql-gdc -e MYSQL_ROOT_PASSWORD=19+Enero+1998 -e MYSQL_DATABASE=gdc -e MYSQL_USER=api -e MYSQL_PASSWORD=api..gdc++19  mysql:8.0
+docker build --tag api-gdc --rmi --network dev .
+docker run -d -p 3307:33060 --network dev --rm --name mysql-gdc -e MYSQL_ROOT_PASSWORD=19+Enero+1998 -e MYSQL_DATABASE=gdc -e MYSQL_USER=api -e MYSQL_PASSWORD=api..gdc++19  mysql:8.0
 timeout /t 120
-docker run -d -p 3000:3000 --network dev --name apigdd api-gdd
+docker run -d -p 3000:3000 --network dev --rm --name apigdd api-gdd
 timeout /t 10
-docker run -d -p 5672:5672 --network dev --name mqservice rabbitmq:3.7.17
+docker run -d -p 5672:5672 --network dev --rm --name mqservice rabbitmq:3.7.17
 timeout /t 30
-docker run -d -p 5000:5050 --network dev --name api-gdc api-gdc
+docker run -d -p 5000:5050 --network dev --rm --name api-gdc api-gdc
 timeout /t 10
-docker run --network dev --name mq mq
+docker run --network dev --rm --name mq mq
